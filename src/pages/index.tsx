c825bcd25b1c92f279c40d5ad8e9f0559ffaf027
Martynjuk Ivan <martiniukgomel@gmail.com>
2024-10-01 19:31:13 +0300

    init: create next.js project Tue, Oct  1, 2024  7:31:12 PM

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import localFont from "next/font/local";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Yamaguchi test task</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+    </>
+  );
+}
