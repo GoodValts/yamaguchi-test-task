5dc568ecc2a0bb749d90a5b5d5e091a134c9f60f
Martynjuk Ivan <martiniukgomel@gmail.com>
2024-10-01 23:49:49 +0300

    feat: add favicon Tue, Oct  1, 2024 11:49:49 PM

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <title>Yamaguchi test task</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <main>
         <Input />
